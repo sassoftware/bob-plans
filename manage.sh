ca414f00cfb0f949805d9dd00941862b68b676eb
Michael Tharp <michael.tharp@sas.com>
2014-02-10 19:38:46 +0000

    Add jenkins templates

---
+++ manage.sh
@@ -0,0 +1,79 @@
+#!/bin/bash -e
+
+usage() {
+    echo "usage: $0 {fetch,push} <branch>"
+    exit 1
+}
+
+if [[ -n "$JBUTLER_PATH" ]]
+then
+    export PYTHONPATH="$JBUTLER_PATH:$PYTHONPATH"
+    JBUTLER="$JBUTLER_PATH/commands/jbutler"
+else
+    JBUTLER=jbutler
+fi
+
+cmd=$1
+branch=$2
+[[ -z "$branch" ]] && usage
+
+case "$cmd" in
+    fetch)
+        # Pull all jobs for the branch down and strip out the branch name
+        rm -rf jobs jobs-clean
+        mkdir jobs jobs-clean
+        $JBUTLER jobs retrieve --filter="rbuilder-${branch}_.*"
+        for job in jobs/*.xml
+        do
+            name=$(echo $(basename $job) | sed -e "s/${branch}/@BRANCH@/g")
+            sed -e "s/$branch/@BRANCH@/g" "$job" | tidy -i -xml -quiet -wrap 0 >"jobs-clean/$name"
+        done
+        git add jobs-clean
+        git add -u jobs-clean
+        git status
+        echo "Retrieved all jobs in branch '$branch' and updated the git index"
+        ;;
+
+    update-deps)
+        # Rewrite SCM watch paths and inter-job dependencies to match the .bob files
+        bob-deps centos-6n --scm --graph >meta/graph.py
+        python meta/jenkins_deps.py
+        git add -u jobs-clean
+        git status
+        echo "Deps updated, now run './manage.sh push $branch' and 'git commit'"
+        ;;
+
+    push)
+        # Substitute a branch name into the generic jobs-clean templates and push it to jenkins
+        rm -rf jobs
+        mkdir jobs
+        $JBUTLER jobs retrieve --filter="rbuilder-${branch}_.*"
+        existing=$(mktemp)
+        ls jobs >"$existing"
+
+        for job in jobs-clean/*.xml
+        do
+            name=$(echo $(basename $job) | sed -e "s/@BRANCH@/${branch}/g")
+            sed -e "s/@BRANCH@/$branch/g" "$job" >"jobs/$name"
+            if grep -q "^$name$" "$existing"
+            then
+                $JBUTLER jobs update "jobs/$name"
+            else
+                $JBUTLER jobs create "jobs/$name"
+            fi
+            rm -f "jobs/$name"
+        done
+        echo "Pushed all jobs in branch '$branch' to jenkins"
+        extra=$(ls jobs)
+        if [[ -n "$extra" ]]
+        then
+            echo "The following jobs are not currently tracked: $extra"
+            echo "Either delete them or use './manage.sh fetch $branch' to pull them in."
+        fi
+        ;;
+
+    *)
+        usage
+        ;;
+esac
+exit 0
---
+++ meta/jenkins_deps.py
@@ -0,0 +1,119 @@
+#!/usr/bin/python
+#
+# Copyright (c) SAS Institute Inc.
+#
+
+import os
+import subprocess
+import sys
+from lxml import etree
+try:
+    from graph import dep_graph, scm_deps
+except ImportError:
+    sys.exit("You must run bob-deps --graph --scm yourplans >graph.py")
+
+
+def _url_from_plan(xml):
+    "Extract the bob plan that this job builds."
+    for builder in xml.find('builders'):
+        if builder.tag != 'hudson.tasks.Shell':
+            return None
+        cmd = builder.find('command').text.split()
+        if cmd[0].split('/')[-1] not in ('bob', 'bob-4.0', 'bob-jenkins', 'bob-jenkins-4.0'):
+            continue
+        for arg in cmd[1:]:
+            if arg.endswith('.bob'):
+                return arg
+    return None
+
+
+def _find_job(url):
+    chunks = url.split('/')
+    for n in range(len(chunks) - 1, -1, -1):
+        path = '/'.join(chunks[n:])
+        if os.path.exists(path):
+            return path
+    raise RuntimeError("Couldn't find %s in working directory" % url)
+
+
+def main():
+    top = 'jobs-clean'
+    forest = 'scc/appengine'
+    jobs = os.listdir(top)
+    plan_to_job = {}
+    job_to_plan = {}
+    after = {}
+    jobs_xml = {}
+    to_write = set()
+    for job in sorted(jobs):
+        if not job.endswith('.xml'):
+            continue
+        with open(os.path.join(top, job)) as f:
+            xml = etree.parse(f)
+        url = _url_from_plan(xml)
+        if not url:
+            continue
+        jobs_xml[job] = xml
+        plan = os.path.basename(_find_job(url))
+        plan_to_job[plan] = job
+        job_to_plan[job] = plan
+        for prov_plan in dep_graph[plan]:
+            after.setdefault(job, set()).add(prov_plan)
+
+    # dependencies
+    for before_job, after_plans in after.iteritems():
+        after_jobs = sorted(plan_to_job[x] for x in after_plans
+                if x in plan_to_job)
+        xml = jobs_xml[before_job]
+
+        publishers = xml.find('publishers')
+        children = set(x.rsplit('.', 1)[0] for x in after_jobs)
+        for publisher in publishers.getchildren():
+            if publisher.tag == 'hudson.tasks.BuildTrigger':
+                children.update(publisher.find('childProjects').text.split(', '))
+                publishers.remove(publisher)
+        publisher = etree.SubElement(publishers, 'hudson.tasks.BuildTrigger')
+        etree.SubElement(publisher, 'childProjects').text = ', '.join(sorted(children))
+        threshold = etree.SubElement(publisher, 'threshold')
+        etree.SubElement(threshold, 'name').text = 'SUCCESS'
+        etree.SubElement(threshold, 'ordinal').text = '0'
+        etree.SubElement(threshold, 'color').text = 'BLUE'
+        etree.SubElement(threshold, 'completeBuild').text = 'true'
+
+        to_write.add(before_job)
+
+    # wms paths
+    for job, xml in jobs_xml.items():
+        plan = job_to_plan[job]
+        paths = scm_deps.get(plan)
+        if not paths:
+            continue
+
+        project = xml.getroot()
+        scm = xml.find('scm')
+        if scm is not None:
+            project.remove(scm)
+        scm = etree.SubElement(project, 'scm')
+        scm.attrib['class'] = 'org.jenkinsci.plugins.git_wms.GitWMSAgent'
+        etree.SubElement(scm, 'path').text = forest
+        etree.SubElement(scm, 'branch').text = '@BRANCH@'
+        watchPaths = etree.SubElement(scm, 'watchPaths')
+        for url, path in paths:
+            watchPath = etree.SubElement(watchPaths, 'org.jenkinsci.plugins.git__wms.GitWMSAgent_-WatchPath')
+            etree.SubElement(watchPath, 'watchRepo').text = url
+            etree.SubElement(watchPath, 'watchPath').text = path
+
+        to_write.add(job)
+
+    for job in to_write:
+        xml = jobs_xml[job]
+        out = os.path.join(top, job)
+        with open(out + '.tmp', 'w') as f:
+            p = subprocess.Popen(['tidy -i -xml -quiet -wrap 0'],
+                    stdin=subprocess.PIPE, stdout=f, shell=True)
+            p.communicate(etree.tostring(xml))
+            if p.wait():
+                sys.exit("tidy failed")
+        os.rename(out + '.tmp', out)
+
+main()
